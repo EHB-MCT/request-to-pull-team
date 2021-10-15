aba616c34b35c41fca723a7352195f102b0ec4ca
IbrahimBurakGozen <ibrahim.gozen@student.ehb.be>
2021-10-15 16:27:42 +0200

    feature(server.js): new get endpoint made

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = 4000;
 
 app.use(express.json());
 
+app.get("/api/persoon1", (req, res) => {
+    res.send("Thanksss this is person 1");
+})
+
 app.listen(PORT, () => {
     console.log("server is listening on port: ", PORT);
 })
