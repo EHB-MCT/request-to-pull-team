c762b07640110b907854aa488a63f70053766be4
Burak Gozen <Burak@MacBook-Pro-van-Burak.local>
2021-10-15 16:41:39 +0200

    feature(server.js): new get api endpoint added

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/api/persoon3", (req, res) => {
     res.send("Thanksss this is person 3");
 })
 
+app.get("/api/persoon4", (req, res) => {
+    res.send("Thanksss this is person 4");
+})
+
 
 app.listen(PORT, () => {
     console.log("server is listening on port: ", PORT);
