3c9a1b83b20445a836f358fb6f7c76db4b350f87
Burak Gozen <Burak@MacBook-Pro-van-Burak.local>
2021-10-15 16:48:49 +0200

    feature(api): new get api endpoint

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const PORT = 4000;
 
 app.use(express.json());
 
+app.get("/api/persoon1", (req, res) => {
+    res.send("Thanksss this is person 1");
+})
 
 app.get("/api/persoon2", (req, res) => {
     res.send("Thanksss this is person 2");
+})
+
+app.get("/api/persoon3", (req, res) => {
+    res.send("Thanksss this is person 3");
+})
+
+app.get("/api/persoon4", (req, res) => {
+    res.send("Thanksss this is person 4");
 })
 
 
